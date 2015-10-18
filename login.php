482fdf24c1a87c51ad06662e3ef4d868c59a3068
Julianne D Johnson <jaydeejaye@gmail.com>
2015-10-18 16:26:15 -0400

    Initial commit, including Udeme's code and contributors.txt

---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Activity Tracker</title>
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/"></script>
+    </head>
+
+    <body>
+        <div class="container">
+
+    	<div class = "Login-box">
+        
+        <form>
+        <legend>Login</legend>
+        <h4>Username:</h4>
+        <input type="text" name="firstname">
+        <br>
+        <h4>Password:</h4>
+        <input type="password" name="lastname">
+         <br>
+         <h4>
+        <input type="Submit" value="Sign Up">
+        <input type="Submit" value="Login">
+        </h4>
+
+        </form> 
+        </div>
+
+    </div>
+    </body>
+    </html>
---
+++ signup.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Activity Tracker</title>
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/"></script>
+    </head>
+
+    <body>
+        <div class="container">
+
+        <div class = "Signup-box">
+        
+        <form>    
+        <legend>Sign up</legend>
+        <h4>Username:</h4>
+        <input type="text" name="firstname">
+        <br>
+        <h4>Password:</h4>
+        <input type="password" name="lastname">
+         <br>
+         <h4>Confirm Password:</h4>
+        <input type="password" name="con-password">
+         <br>
+         <h4>Organization name:</h4>
+        <input type="text" name="org-name">
+         <br>
+         <h4>
+        <input type="Submit" value="Submit">
+        </h4>
+
+        </form> 
+        </div>
+
+    </div>
+    </body>
+    </html>
